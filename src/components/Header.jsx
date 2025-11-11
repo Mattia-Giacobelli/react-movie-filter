445ff40c8af182a9df81503c274ec8d53771133e
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-11-11 12:33:37 +0100

    Create Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,13 @@ export default function Header() {
 
     return (
 
-        <header>
+        <header className="bg-primary-subtle">
+            <div className="container ">
+                <h1 className="text-center pt-5 pb-4">
+                    MOVIE LIST
+                </h1>
 
+            </div>
         </header>
 
     )
