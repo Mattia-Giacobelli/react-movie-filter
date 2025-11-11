769c3fda6e3cffcc3fc64e989df60709844ec7bd
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-11-11 12:26:22 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <Header />
+
+      <Main />
+
+      <Footer />
+
+    </>
+  )
+}
+
+export default App
